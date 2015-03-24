cd0d52301a9527f1a3c72f9edf53dd740dba25a6
liubin <flylb1@gmail.com>
2015-03-24 20:20:34 +0800

    Add CopyRight

--- SerialPortSDK/src/serialportutil/SerialPortUtil.java
+++ SerialPortSDK/src/serialportutil/SerialPortUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 
+ * Copyright 2015 Bin Liu (flylb1@gmail.com)
  *   
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
